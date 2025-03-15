d745a9ba985bb1818dabab196e2197b28508c494
Eric Polanski <ericpolanski2025@u.northwestern.edu>
2025-03-14 23:17:02 -0500

    Console shows AI responses, removed text option, inserted our prompt, changed silence_duration

--- client/components/SessionControls.jsx
+++ client/components/SessionControls.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { CloudLightning, CloudOff, MessageSquare } from "react-feather";
+import { Play, Pause } from "react-feather";
 import Button from "./Button";
 
 function SessionStopped({ startSession }) {
@@ -17,49 +17,19 @@ function SessionStopped({ startSession }) {
       <Button
         onClick={handleStartSession}
         className={isActivating ? "bg-gray-600" : "bg-red-600"}
-        icon={<CloudLightning height={16} />}
+        icon={<Play height={16} />}
       >
-        {isActivating ? "starting session..." : "start session"}
+        {isActivating ? "Starting Session..." : "Start Session"}
       </Button>
     </div>
   );
 }
 
-function SessionActive({ stopSession, sendTextMessage }) {
-  const [message, setMessage] = useState("");
-
-  function handleSendClientEvent() {
-    sendTextMessage(message);
-    setMessage("");
-  }
-
+function SessionActive({ stopSession }) {
   return (
     <div className="flex items-center justify-center w-full h-full gap-4">
-      <input
-        onKeyDown={(e) => {
-          if (e.key === "Enter" && message.trim()) {
-            handleSendClientEvent();
-          }
-        }}
-        type="text"
-        placeholder="send a text message..."
-        className="border border-gray-200 rounded-full p-4 flex-1"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <Button
-        onClick={() => {
-          if (message.trim()) {
-            handleSendClientEvent();
-          }
-        }}
-        icon={<MessageSquare height={16} />}
-        className="bg-blue-400"
-      >
-        send text
-      </Button>
-      <Button onClick={stopSession} icon={<CloudOff height={16} />}>
-        disconnect
+      <Button onClick={stopSession} icon={<Pause height={16} />}>
+        Disconnect
       </Button>
     </div>
   );
@@ -68,20 +38,12 @@ function SessionActive({ stopSession, sendTextMessage }) {
 export default function SessionControls({
   startSession,
   stopSession,
-  sendClientEvent,
-  sendTextMessage,
-  serverEvents,
   isSessionActive,
 }) {
   return (
     <div className="flex gap-4 border-t-2 border-gray-200 h-full rounded-md">
       {isSessionActive ? (
-        <SessionActive
-          stopSession={stopSession}
-          sendClientEvent={sendClientEvent}
-          sendTextMessage={sendTextMessage}
-          serverEvents={serverEvents}
-        />
+        <SessionActive stopSession={stopSession} />
       ) : (
         <SessionStopped startSession={startSession} />
       )}
